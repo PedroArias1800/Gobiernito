cd7ee57761186824820ce3c6b560d2778c31c59f
PedroArias1800 <pedrosoftdev18@gmail.com>
2023-11-21 18:20:11 -0500

    Logica para ir sumando personajes

--- src/pages/EresPrd.jsx
+++ src/pages/EresPrd.jsx
@@ -1,13 +1,27 @@
 import React, { useState } from 'react'
 import Personaje1 from '../../public/img/Personaje1.jpg'
+import Personaje2 from '../../public/img/Personaje2.jpg'
+import Personaje3 from '../../public/img/Personaje3.jpg'
+import Personaje4 from '../../public/img/Personaje4.jpg'
 
 export const EresPrd = () => {
     const [brincos, setBrincos] = useState(0)
     const [isJumping, setJumping] = useState(false);
+    const [personajes, setPersonajes] = useState([Personaje1])
+    const Brincos100 = [Personaje1,Personaje2]
+    const Brincos200 = [Personaje1,Personaje2,Personaje3]
+    const Brincos300 = [Personaje1,Personaje2,Personaje3,Personaje4]
 
     const handleJump = () => {
         setJumping(true);
         setBrincos(brincos+1)
+        if (brincos == 20){
+            setPersonajes(Brincos100)
+        } else if (brincos == 40){
+            setPersonajes(Brincos200)
+        } else if (brincos == 50){
+            setPersonajes(Brincos300)
+        } 
 
         // Después de medio segundo, vuelve a establecer el estado a falso
         setTimeout(() => {
@@ -19,7 +33,9 @@ export const EresPrd = () => {
     <>
         <h3>Total de Brincos: {brincos}</h3>
         <div id='brincar' className={`box ${isJumping ? 'jump' : ''}`} onClick={handleJump}>
-            <img src={Personaje1} alt='' />
+            {personajes.map(p => (
+                <img src={p} alt='' />
+            ))}
         </div>
         <div id='suelo'>PRD</div>
     </>
